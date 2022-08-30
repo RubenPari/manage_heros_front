1c455ec6232ee619605155a7f954f25f4a099aec
RubenPari <rubenpari.developer@gmail.com>
2022-08-30 22:05:01 +0200

    added bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './app/App';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
