ae30521961a44a27b603cda98fbf53769a542cde
RubenPari <rubenpari.developer@gmail.com>
2022-09-01 22:24:48 +0200

    completed AddHero comp

--- src/components/AddHero.tsx
+++ src/components/AddHero.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import axios from 'axios';
 import Hero from '../models/Hero';
 
+
 async function getAllHeroes() {
     return await axios.get('http://localhost:8000/heroes/get_all_available');
 }
 
-class AddHero extends React.Component<{}, { heroes: Hero[] }> {
+class AddHero extends React.Component<{}, { heroes: Hero[], heroName: string, response: string }> {
     constructor(props: any) {
         super(props);
         this.state = {
             heroes: Array<Hero>(),
+            heroName: "",
+            response: "",
         }
+        this.setNameHero = this.setNameHero.bind(this);
     }
 
     componentDidMount() {
@@ -25,15 +29,34 @@ class AddHero extends React.Component<{}, { heroes: Hero[] }> {
             });
     }
 
+    setNameHero(event: any) {
+        this.setState({heroName: event.target.value});
+    }
+
+    addHero = () => {
+        axios.post('http://localhost:8000/heroes/add?name=' + this.state.heroName)
+            .then(() => {
+                this.setState({response: "Hero added"});
+            }).catch((err) => {
+                console.log('Error to add hero');
+                console.log(err);
+            }
+        );
+    }
+
     render() {
         return (
             <div>
-                <select name="dropdown">
+                <h3>Select hero that you would add</h3>
+                <select name="dropdown" onChange={this.setNameHero}>
                     {this.state.heroes.map((hero) => {
-                        return <option value={hero.id}>{hero.name}</option>
+                        return <option key={hero.id} value={hero.name}>{hero.name}</option>
                     })}
                 </select>
-                <button className="submit">Invia</button>
+                <button className="submit" onClick={this.addHero}>Send</button>
+                <h4>
+                    {this.state.response}
+                </h4>
             </div>
         );
     }
